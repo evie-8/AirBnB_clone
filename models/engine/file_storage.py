8cab8028a479240cbcfd5391da6b43982ddbfbf9
evie-8 <neouma29@gmail.com>
2023-07-15 17:41:40 +0300

    finding errors in file_storage file and correcting them

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -27,19 +27,21 @@ class FileStorage:
     def save(self):
         """Serialize __objects to the JSON file __file_path."""
         data = {}
-        for key, value in self.__objects.items():
+        for key, value in FileStorage.__objects.items():
             data[key] = value.to_dict()
         with open(self.__file_path, 'w') as file:
             json.dump(data, file)
 
     def reload(self):
         """Deserialize the JSON file __file_path to __objects, if it exists."""
+        from models.base_model import BaseModel
         try:
             with open(FileStorage.__file_path) as f:
                 objdict = json.load(f)
-                for o in objdict.values():
-                    cls_name = o["__class__"]
-                    del o["__class__"]
-                    self.new(eval(cls_name)(**o))
+                FileStorage._objects = {}
+                for k, v in objdict.items():
+                    cls_name = v["__class__"]
+                    # del o["__class__"]
+                    FileStorage.__objects[k] = eval(cls_name)(**v)
         except FileNotFoundError:
             return
